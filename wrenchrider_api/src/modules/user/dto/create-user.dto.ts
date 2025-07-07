90de58b2a3e7a1c915262fee03c48ffc97993488
Blazkowicz <bonnet.nicolas66@gmail.com>
2025-07-07 23:40:36 +0200

    Add User module

---
+++ wrenchrider_api/src/modules/user/dto/create-user.dto.ts
@@ -0,0 +1,18 @@
+import { IsEmail, IsString, IsBoolean} from 'class-validator';
+
+export class CreateUserDto {
+    @IsString()
+    firstName: string;
+
+    @IsString()
+    lastName: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    password: string;
+
+    @IsBoolean()
+    isAdmin: boolean;
+}
