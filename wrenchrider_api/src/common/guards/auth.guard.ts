8ee713e446b748d508359b41d0d6fe3b0af6c175
Blazkowicz <bonnet.nicolas66@gmail.com>
2025-07-09 18:18:47 +0200

    Update auth guard

--- wrenchrider_api/src/common/guards/auth.guard.ts
+++ wrenchrider_api/src/common/guards/auth.guard.ts
@@ -1,27 +1,37 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+	CanActivate,
+	ExecutionContext,
+	Injectable,
+	UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { User } from '../../modules/user/user.entity';
-import { Request, Response } from 'express';
-import { Observable } from 'rxjs';
+import { Request } from 'express';
+
+interface JwtPayload {
+	id: string;
+	isAdmin: boolean;
+}
 
 /**
  * Check is User is connected
  */
 @Injectable()
 export class IsAuthGuard implements CanActivate {
-	constructor(private readonly jwtService: JwtService) {
-	}
+	constructor(private readonly jwtService: JwtService) {}
 
 	canActivate(context: ExecutionContext): boolean {
 		const request = context.switchToHttp().getRequest<Request>();
 		const token = request.headers.authorization;
 
 		if (!token) {
-			throw new UnauthorizedException('Missing or invalid Authorization header');
+			throw new UnauthorizedException(
+				'Missing or invalid Authorization header',
+			);
 		}
 
 		try {
-			request['user'] = this.jwtService.verify(token);
+			request['user'] = this.jwtService.verify<JwtPayload>(token);
 			return true;
 		} catch (error) {
 			console.error(error);
@@ -35,8 +45,7 @@ export class IsAuthGuard implements CanActivate {
  */
 @Injectable()
 export class IsAdminGuard implements CanActivate {
-	constructor(private readonly jwtService: JwtService) {
-	}
+	constructor(private readonly jwtService: JwtService) {}
 
 	canActivate(context: ExecutionContext): boolean {
 		const request = context.switchToHttp().getRequest<Request>();
