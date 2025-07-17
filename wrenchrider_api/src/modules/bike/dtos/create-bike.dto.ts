d9d98a85792bbaae585d7fc1a1132c2b95c6637c
Blazkowicz <bonnet.nicolas66@gmail.com>
2025-07-17 17:20:28 +0200

    Update bike module

--- wrenchrider_api/src/modules/bike/dtos/create-bike.dto.ts
+++ wrenchrider_api/src/modules/bike/dtos/create-bike.dto.ts
@@ -1,15 +1,15 @@
 import { IsString, IsNumber, IsObject } from 'class-validator';
 
 export class CreateBikeDto {
-    @IsString()
-    name: string;
+	@IsString()
+	name: string;
 
-    @IsObject()
-    brand: object
+	@IsObject()
+	brand: object;
 
-    @IsNumber()
-    power: number
+	@IsNumber()
+	power: number;
 
-    @IsNumber()
-    year: number
+	@IsNumber()
+	year: number;
 }
