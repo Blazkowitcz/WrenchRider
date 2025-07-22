8172cba3d51e8ad425a233c843abd9e21621ba21
Blazkowicz <bonnet.nicolas66@gmail.com>
2025-07-22 11:40:18 +0200

    Add new userBikes routes

--- wrenchrider_api/src/modules/user-bike/user-bike.controller.ts
+++ wrenchrider_api/src/modules/user-bike/user-bike.controller.ts
@@ -1,15 +1,19 @@
 import {
 	Controller,
 	Post,
+	Put,
 	Body,
 	HttpException,
 	HttpStatus,
 	Get,
 	Req,
 	UseGuards,
+	Param,
+	Delete,
 } from '@nestjs/common';
 import { UserBikeService } from './user-bike.service';
 import { CreateUserBikeDto } from './dtos/create-user-bike.dto';
+import { EditUserBikeDto } from './dtos/edit-user-bike.dto';
 import { UserBike } from './user-bike.entity';
 import { UserRequest } from '../user/user.entity';
 import { IsAuthGuard } from '../../common/guards/auth.guard';
@@ -19,9 +23,17 @@ export class UserBikeController {
 	constructor(private readonly userBikeService: UserBikeService) {
 	}
 
+	/**
+	 * Add user bike
+	 * @param request {UserRequest}
+	 * @param dto {CreateUserBikeDto}
+	 */
 	@UseGuards(IsAuthGuard)
 	@Post()
-	async addUserBike(@Req() request: UserRequest, @Body() dto: CreateUserBikeDto): Promise<UserBike> {
+	async addUserBike(
+		@Req() request: UserRequest,
+		@Body() dto: CreateUserBikeDto,
+	): Promise<UserBike> {
 		try {
 			return this.userBikeService.addUserBike({
 				user: request.user,
@@ -38,11 +50,61 @@ export class UserBikeController {
 		}
 	}
 
+	/**
+	 * Get all bikes from user
+	 * @param request {UserRequest}
+	 */
 	@UseGuards(IsAuthGuard)
 	@Get()
 	async getAllBikesFromUser(
 		@Req() request: UserRequest,
 	): Promise<UserBike[]> {
 		return this.userBikeService.getAllBikesFromUser(request.user.id);
+	}
+
+	/**
+	 * Edit user bike
+	 * @param userBikeId {String}
+	 * @param request {UserRequest}
+	 * @param dto {EditUserBikeDto}
+	 */
+	@UseGuards(IsAuthGuard)
+	@Put(':userBikeId')
+	async editUserBike(
+		@Param('userBikeId') userBikeId: string,
+		@Req() request: UserRequest,
+		@Body() dto: EditUserBikeDto,
+	): Promise<boolean> {
+		const userBike = await this.userBikeService.getUserBike(
+			request.user.id,
+			userBikeId,
+		);
+		if (userBike) {
+			await this.userBikeService.editUserBike(userBike, dto);
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 *
+	 * @param userBikeId
+	 * @param request
+	 */
+	@UseGuards(IsAuthGuard)
+	@Delete(':userBikeId')
+	async removeUserBike(
+		@Param('userBikeId') userBikeId: string,
+		@Req() request: UserRequest,
+	): Promise<boolean> {
+		const userBike = await this.userBikeService.getUserBike(
+			request.user.id,
+			userBikeId,
+		);
+		if (userBike) {
+			await this.userBikeService.removeUserBike(userBike);
+			return true;
+		}
+		return false;
 	}
 }
